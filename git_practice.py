b3f07551ccb59a72eb4715a76039f70197c37b45
Baburaj <sbrbabu123@gmail.com>
2024-10-09 16:18:24 +0530

    Finished my work

--- git_practice.py
+++ git_practice.py
@@ -3,5 +3,11 @@ print()
 print("Hello Git")
 print("Go to Rest")
 
+# Working on my Task
+def function():
+    return 0
+
+print(function())
+
 # Working on Urgent fix
 print("Urgent work Finished")
